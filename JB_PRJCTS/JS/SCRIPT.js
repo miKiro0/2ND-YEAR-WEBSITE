b664ca1593aadc9605d8376d07e09a4608b68d67
JEFFREY DAELLA BAUTISTA <127912773+miKiro0@users.noreply.github.com>
2024-10-06 18:08:14 +0800

    update this after u made some changes

---
+++ JB_PRJCTS/JS/SCRIPT.js
@@ -0,0 +1,8 @@
+<script>
+    function w3_open() {
+        document.getElementById("mySideBar").style.display = "block";
+    }
+    function w3_close(){
+        document.getElementById("mySideBar").style.display = "none";
+    }
+</script>
